eabd4c4f846ed1ac75febc9bc53916976d9d31cf
Kenneth Carter <kccarter32@gmail.com>
2019-12-11 15:48:51 -0700

    no message

--- SubSonic/Linq/Microsoft/ExpressionWriter.cs
+++ SubSonic/Linq/Microsoft/ExpressionWriter.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// This source code is made available under the terms of the Microsoft Public License (MS-PL)
+//Original code created by Matt Warren: http://iqtoolkit.codeplex.com/Release/ProjectReleases.aspx?ReleaseId=19725
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
