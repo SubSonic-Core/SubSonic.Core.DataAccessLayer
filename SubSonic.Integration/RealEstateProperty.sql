02e6f975387dfff043f454d75b3077978e68a560
Carter, Kenneth <kccarter32@gmail.com>
2020-01-07 08:37:44 -0700

    created a integration project for creating a real SQL Server database to facilitate integration testing

---
+++ SubSonic.Integration/RealEstateProperty.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[RealEstateProperty]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [StatusId] INT NOT NULL, 
+    [HasParallelPowerGeneration] BIT NULL
+) WITH (DATA_COMPRESSION=PAGE)
+
+GO
+
+CREATE COLUMNSTORE INDEX [CStoreIX_RealEstateProperty] ON [dbo].[RealEstateProperty] ([Id], [StatusId], [HasParallelPowerGeneration])
