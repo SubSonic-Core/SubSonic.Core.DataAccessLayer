f4fb416f6309b56425ac670221697159859f3b13
Kenneth Carter <kccarter32@gmail.com>
2020-01-07 20:06:57 -0700

    removed the integration project

--- SubSonic.Integration/Models/RealEstateProperty/DeleteRealEstateProperty.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[DeleteRealEstateProperty]
-	@properties [dbo].[RealEstateProperty] readonly
-AS
-
-	DELETE
-	FROM [dbo].[RealEstateProperty]
-	WHERE [Id] IN (SELECT [ID] FROM @properties);
-
-RETURN 0
