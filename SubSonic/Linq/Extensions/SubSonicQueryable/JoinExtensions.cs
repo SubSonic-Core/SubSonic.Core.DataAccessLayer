52e1a98ad6dbe3595db469b3b1cfb24cdc20901f
Kenneth Carter <kccarter32@gmail.com>
2020-01-10 16:26:25 -0700

    no message

--- SubSonic/Linq/Extensions/SubSonicQueryable/JoinExtensions.cs
+++ SubSonic/Linq/Extensions/SubSonicQueryable/JoinExtensions.cs
@@ -10,7 +10,32 @@ namespace SubSonic.Linq
 
     public static partial class SubSonicQueryable
     {
-        public static IQueryable<TSource> Join<TSource>(this IQueryable<TSource> source, JoinType type, DbExpression right)
+        public static IQueryable<TSource> CrossApply<TSource>(this IQueryable<TSource> source, DbExpression right)
+        {
+            return source.Join(JoinType.CrossApply, right);
+        }
+
+        public static IQueryable<TSource> CrossJoin<TSource>(this IQueryable<TSource> source, DbExpression right)
+        {
+            return source.Join(JoinType.CrossJoin, right);
+        }
+
+        public static IQueryable<TSource> InnerJoin<TSource>(this IQueryable<TSource> source, DbExpression right)
+        {
+            return source.Join(JoinType.InnerJoin, right);
+        }
+
+        public static IQueryable<TSource> LeftOuter<TSource>(this IQueryable<TSource> source, DbExpression right)
+        {
+            return source.Join(JoinType.LeftOuter, right);
+        }
+
+        public static IQueryable<TSource> OuterApply<TSource>(this IQueryable<TSource> source, DbExpression right)
+        {
+            return source.Join(JoinType.OuterApply, right);
+        }
+
+        internal static IQueryable<TSource> Join<TSource>(this IQueryable<TSource> source, JoinType type, DbExpression right)
         {
             if (source.IsNotNull() && source.IsSubSonicQuerable())
             {
