f2602e4f9156e9d57f511033a76df3a24d19aeab
Kenneth Carter <kccarter32@gmail.com>
2020-06-11 16:59:52 -0600

    no message

---
+++ SubSonic/Linq/Expressions/Structure/SqlFormatter/TSqlFormatterVisitInsert.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SubSonic.Linq.Expressions.Structure
+{
+    public partial class TSqlFormatter
+    {
+        protected internal override Expression VisitInsert(DbInsertExpression insert)
+        {
+            if (insert.IsNotNull())
+            {
+                return base.VisitInsert(insert);
+            }
+
+            return null;
+        }
+    }
+}
