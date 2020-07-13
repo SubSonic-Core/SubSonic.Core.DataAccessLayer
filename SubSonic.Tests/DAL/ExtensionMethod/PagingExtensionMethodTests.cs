9b1b8619bc80d10ffd3f1b5aeb0e18c9e8ee0666
Kenneth Carter <kccarter32@gmail.com>
2020-07-12 21:52:00 -0600

    no message

---
+++ SubSonic.Tests/DAL/ExtensionMethod/PagingExtensionMethodTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubSonic.Tests.DAL.ExtensionMethod
+{
+    public partial class ExtensionMethodTests
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            var answer = 42;
+            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+        }
+    }
+}
