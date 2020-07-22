6bb94801be93a41f9c2114e00d48c66fc0a55ecc
Kenneth Carter <kccarter32@gmail.com>
2020-07-21 23:34:59 -0600

    #58

---
+++ SubSonic.Core.TemplateWizard/Wizards/DataContextWizard.cs
@@ -0,0 +1,66 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SubSonic.Core.Template.Wizards
+{
+    using Forms;
+
+    public class DataContextWizard
+        : IWizard
+    {
+        private UserInputForm inputForm;
+        private string customMessage;
+
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+            
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+            
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+            
+        }
+
+        public void RunFinished()
+        {
+            
+        }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            try
+            {
+                // Display a form to the user. The form collects
+                // input for the custom message.
+                inputForm = new UserInputForm();
+                inputForm.ShowDialog();
+
+                customMessage = UserInputForm.CustomMessage;
+
+                // Add custom parameters.
+                replacementsDictionary.Add("$custommessage$",
+                    customMessage);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+    }
+}
--- SubSonic.Core.VisualStudio/SubSonic.Core.VisualStudioPackage.cs
+++ SubSonic.Core.VisualStudio/SubSonic.Core.VisualStudioPackage.cs
@@ -24,30 +24,30 @@ namespace SubSonic.Core.VisualStudio
     /// </para>
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
-    [Guid(SubSonic.Core.VisualStudioPackage.PackageGuidString)]
-    public sealed class SubSonic.Core.VisualStudioPackage : AsyncPackage
+    [Guid(SubSonicCoreVisualStudioPackage.PackageGuidString)]
+    public sealed class SubSonicCoreVisualStudioPackage : AsyncPackage
     {
-         /// <summary>
+        /// <summary>
         /// SubSonic.Core.VisualStudioPackage GUID string.
         /// </summary>
         public const string PackageGuidString = "fb84c14b-ef14-43ce-810a-1f6d1b3fbefb";
 
-    #region Package Members
+        #region Package Members
 
-    /// <summary>
-    /// Initialization of the package; this method is called right after the package is sited, so this is the place
-    /// where you can put all the initialization code that rely on services provided by VisualStudio.
-    /// </summary>
-    /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
-    /// <param name="progress">A provider for progress updates.</param>
-    /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
-    protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
-    {
-        // When initialized asynchronously, the current thread may be a background thread at this point.
-        // Do any initialization that requires the UI thread after switching to the UI thread.
-        await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
+        /// <param name="progress">A provider for progress updates.</param>
+        /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            // When initialized asynchronously, the current thread may be a background thread at this point.
+            // Do any initialization that requires the UI thread after switching to the UI thread.
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        }
+
+        #endregion
     }
-
-    #endregion
-}
 }
